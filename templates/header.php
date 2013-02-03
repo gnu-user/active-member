3980ab283437bea11859e0fecac4f2c8e46a2c34
gnu_user <gsoc.student@gmail.com>
2013-02-03 02:30:37 -0500

    Added bootswatch and custom CSS files

--- templates/header.php
+++ templates/header.php
@@ -32,6 +32,8 @@
     <!-- CSS styles -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/bootswatch.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
