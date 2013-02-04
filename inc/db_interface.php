f654f74bc3398fb1d99ab29d5ca53499936ebbac
gnu_user <gsoc.student@gmail.com>
2013-02-03 23:17:00 -0500

    Added a function to add members to active table

--- inc/db_interface.php
+++ inc/db_interface.php
@@ -24,7 +24,7 @@
  * exists in the registered members table. Use this method to verify that the name
  * and student number provided belongs to a registered club member.
  * 
- * @param mysqli $mysqli_accounts The mysqli connection object for the ucsc accounts DB
+ * @param mysqli $mysqli_conn The mysqli connection object
  * @param string $first_name the first name of the individual
  * @param string $last_name the last name of the individual
  * @param string $student_number the student number of the individual
@@ -84,123 +84,76 @@ function verify_member($mysqli_conn, $first_name, $last_name, $student_number, $
 }
 
 
+/**
+ * A Function which determines if a user has already been set as active
+ *
+ * @param mysqli $mysqli_conn The mysqli connection object
+ * @param int $access_Account The unique (primary key) access account number of the user
+ * @return boolean True if the user is already active
+ */
+function is_active($mysqli_conn, $access_account)
+{
+    /* Set the active members table for current year */
+    date_default_timezone_set('America/Toronto');
+    $active_table = 'active_' . date('Y');
+     
+    if ($stmt = $mysqli_conn->prepare("SELECT 
+                                                EXISTS(
+                                                    SELECT 1
+                                                    FROM "
+                                                        . $active_table . 
+                                                    " WHERE 
+                                                        access_account=?
+                                                )"))
+                {
+        /* bind parameters for markers */
+        $stmt->bind_param('i', $access_account);
+
+        /* execute query */
+        $stmt->execute();
+
+        /* bind result variables */
+        $stmt->bind_result($is_active);
+
+        /* fetch value */
+        $stmt->fetch();
+
+        /* close statement */
+        $stmt->close();
+    }
+
+    return $is_active;
+}
+
 
 /**
- * TODO test and make sure it actually works
- * Used to check if a given nominee (with their first, last and user name) is a nominee
- * capable to be elected for the given position.
+ * A function which adds a person who is an active member to the active 
+ * members table if they do not already exist in the table.
  *
- * @param mysqli $mysqli_elections The mysqli connection object for the ucsc elections DB
- * @param string $nominee the full name of the nominee
- * @param string $user_name the user name of a potential nominee
- * @param string $position the position that the nominee might hold
- * @return boolean $is_nominee TRUE if the nominee is found in the database with the given position
+ * @param mysqli $mysqli_conn The mysqli connection object
+ * @param string $first_name the first name of the individual
+ * @param string $last_name the last name of the individual
+ * @param int $access_account The unique (primary key) access account number of the user
+ *
  */
-function is_nominee($mysqli_elections, $nominee, $position)
+function add_member($mysqli_conn, $first_name, $last_name, $access_account)
 {
-    $is_nominee = FALSE;
-    
-    $current_year = date('Y');
-    
-    $members_table = "members_" . $current_year;
-    $positions_nom_table = "positions_nom_" . $current_year;
+    /* Set the active members table for current year */
+    date_default_timezone_set('America/Toronto');
+    $active_table = 'active_' . date('Y');
 
-    if ($stmt = $mysqli_elections->prepare("SELECT EXISTS(
-                                                    SELECT 1 FROM ".$members_table." m INNER JOIN ".$positions_nom_table.
-                                                        " p ON p.reference = m.access_account WHERE
-                                                            p.position LIKE ? AND CONCAT
-                                                                (
-                                                                    m.first_name,
-                                                                    ' ',
-                                                                    m.last_name
-                                                                )
-                                                                LIKE ?
-                                                             )"))
+    /* Add the user to the active members table */
+    if ($stmt = $mysqli_conn->prepare("INSERT INTO ".$active_table." VALUES (?, ?, ?)"))
     {
         /* bind parameters for markers */
-        $stmt->bind_param('ss', $position, $nominee);
+        $stmt->bind_param('iss', $access_account, $first_name, $last_name);
 
         /* execute query */
         $stmt->execute();
 
-        /* bind result variables */
-        $stmt->bind_result($is_nominee);
-
-        /* fetch value */
-        $stmt->fetch();
-
         /* close statement */
         $stmt->close();
     }
-    return $is_nominee;
 }
-
-
-/** 
- * A function which verifies the login information provided by the user
- * returns true if the login username and password provided are valid
- * 
- * @param mysqli $mysqli_accounts The mysqli connection object for the ucsc accounts DB
- * @param string $username The username of the person logging in
- * @param string $password The password of the person logging in
- * @param string $AES_KEY The AES encrypt/decrypt key for the password
- * @return boolean True if the login information provided is valid
- */
-function verify_login($mysqli_accounts, $username, $password, $AES_KEY)
-{
-    $user_match = '';
-    $pass_match = '';
-    
-    /* Get the username from the database if it exists */
-    if ($stmt = $mysqli_accounts->prepare("SELECT username FROM ucsc_members WHERE username LIKE ?"))
-    {
-        /* bind parameters for markers */
-        $stmt->bind_param('s', $username);
-
-        /* execute query */
-        $stmt->execute();
-
-        /* bind result variables */
-        $stmt->bind_result($user_match);
-
-        /* fetch value */
-        $stmt->fetch();
-
-        /* close statement */
-        $stmt->close();
-    }
-    
-    /* If username found, verify the password provided for that username */
-    if (strcasecmp($username, $user_match) === 0)
-    {
-        if ($stmt = $mysqli_accounts->prepare("SELECT AES_DECRYPT(password, ?) FROM ucsc_members WHERE username LIKE ?"))
-        {
-            /* bind parameters for markers */
-            $stmt->bind_param('ss', $AES_KEY, $username);
-    
-            /* execute query */
-            $stmt->execute();
-    
-            /* bind result variables */
-            $stmt->bind_result($pass_match);
-    
-            /* fetch value */
-            $stmt->fetch();
-    
-            /* close statement */
-            $stmt->close();
-        }
-        /* Verify the password, remove the salt from password stored in DB */
-       if (strcmp($password, substr($pass_match, 8)) === 0)
-       {
-           return true;
-       }
-    }
-  
-  /* Invalid username or password or both */
-    return false;
-}
-
-
 
 ?>
