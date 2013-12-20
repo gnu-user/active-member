9d23c264f3e7516b1e9f6be3091255b2cd8e2ef9
gnu_user <gsoc.student@gmail.com>
2013-12-19 23:58:27 -0500

    Cleanup: Set to use minified bootswatch file

--- templates/header.php
+++ templates/header.php
@@ -32,7 +32,7 @@
     <!-- CSS styles -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-    <link href="css/bootswatch.css" rel="stylesheet">
+    <link href="css/bootswatch.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
